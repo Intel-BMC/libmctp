b65d4d0a719c563714065dcf0ca900cededd4089
Arun P. Mohanan <arun.p.m@linux.intel.com>
2020-06-10 13:00:39 +0530

    Add MCTP Message Type Codes
    Add MCTP Message Type Code macros which are defined in
    DSP0239 v1.3.0 Table 1.
    
    Change-Id: I4f4d490fcbad2c40841a2a07f4600da9af1bc405
    Signed-off-by: Arun P. Mohanan <arun.p.m@linux.intel.com>

---
+++ libmctp-msgtypes.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+
+#ifndef _LIBMCTP_MSGTYPES_H
+#define _LIBMCTP_MSGTYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * MCTP Message Type codes
+ * See DSP0239 v1.3.0 Table 1.
+ */
+#define MCTP_MESSAGE_TYPE_MCTP_CTRL 0x00
+#define MCTP_MESSAGE_TYPE_PLDM 0x01
+#define MCTP_MESSAGE_TYPE_NCSI 0x02
+#define MCTP_MESSAGE_TYPE_ETHERNET 0x03
+#define MCTP_MESSAGE_TYPE_NVME 0x04
+#define MCTP_MESSAGE_TYPE_VDPCI 0x7E
+#define MCTP_MESSAGE_TYPE_VDIANA 0x7f
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIBMCTP_MSGTYPES_H */
