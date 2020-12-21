f5246c429d728ee1b6558692b72ef1960db68a7c
Sabarish Nair <sabarishx.nair@intel.com>
2020-12-21 15:44:49 +0000

    test_smbus: Modify test files for smbus testing
    test_smbus.c modified for testing, the libmctp functions
    for proper operation by using dummy data and simulating the call flow
    
    Tested:
     All unit tests passed
    
    Change-Id: I03f26e68ca59b542ef1f9465eef7b5fe5e2a1b83
    Signed-off-by: Sabarish Nair <sabarishx.nair@intel.com>

--- tests/test_smbus.c
+++ tests/test_smbus.c
@@ -6,6 +6,9 @@
 #include "config.h"
 #endif
 
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
 #include "libmctp-alloc.h"
 #include "libmctp-log.h"
 #include "libmctp-smbus.h"
@@ -21,32 +24,250 @@
 #include <string.h>
 
 #define TEST_EID 8
+#define TEST_TARGET_EID 9
 
-int main(void)
-{
+uint8_t fn_call_cnt;
+
+struct smbus_test_ctx {
+	int fd;
 	struct mctp *mctp;
+	struct mctp_binding_smbus *bind_smbus;
+} __attribute__((packed));
+
+uint8_t payload[] = { 0x01, TEST_EID, TEST_TARGET_EID, 0xB0, 0, 129, 0, 1, 2 };
+
+static struct smbus_test_ctx smbus_ctx = { .fd = -1,
+					   .mctp = NULL,
+					   .bind_smbus = NULL };
+
+static int stubbed_smbus_fd = 5;
+static int16_t pkt_index;
+
+static uint8_t test_packet[][12] = {
+	{ 0x02, 0x0f, 0x069, 0x11, 0x21, 0x08, 0x09, 0x89, 0x00, 0x8a, 0x0b,
+	  0x51 },
+	{ 0x04, 0x0f, 0x069, 0x11, 0x31, 0x08, 0x09, 0x8a, 0x00, 0x8b, 0x0c,
+	  0x61 },
+	{ 0x06, 0x0f, 0x069, 0x11, 0x41, 0x08, 0x09, 0x8b, 0x00, 0x8c, 0x0d,
+	  0x71 },
+	{ 0x0e, 0x0f, 0x069, 0x11, 0x51, 0x08, 0x09, 0x8c, 0x00, 0x8d, 0x0e,
+	  0x81 },
+	{ 0x1e, 0x0f, 0x069, 0x11, 0x61, 0x08, 0x09, 0x8d, 0x00, 0x8e, 0x0f,
+	  0x99 },
+};
+
+int open(const char *_pathname, int _flags)
+{
+	return stubbed_smbus_fd;
+}
+
+int close(int _fd)
+{
+	assert(_fd != stubbed_smbus_fd);
+	return 0;
+}
+
+ssize_t read(int _fd, void *_buf, size_t _count)
+{
+	if (_fd != stubbed_smbus_fd)
+		return -1;
+
+	if (_count > sizeof(test_packet[pkt_index]))
+		_count = sizeof(test_packet[pkt_index]);
+
+	memcpy(_buf, &test_packet[pkt_index], _count);
+
+	pkt_index++;
+
+	if (pkt_index > 4)
+		pkt_index = 0;
+
+	return _count;
+}
+
+ssize_t write(int _fd, void *_data, size_t _data_size)
+{
+	static int write_index;
+
+	if (_fd != stubbed_smbus_fd)
+		return -1;
+
+	return 0;
+}
+
+int ioctl(int _fd, unsigned long _request, void *_data)
+{
+	if (_fd != stubbed_smbus_fd)
+		return -1;
+
+	switch (_request) {
+	case I2C_RDWR: {
+		mctp_smbus_read(smbus_ctx.bind_smbus);
+		break;
+	}
+	default:
+		mctp_prdebug("Unrecognized ioctl");
+		assert(0);
+		break;
+	}
+
+	return 0;
+}
+
+static void init_smbus_test(struct smbus_test_ctx *p_ctx)
+{
 	struct mctp_binding *binding;
-	struct mctp_pktbuf pkt;
 	struct mctp_binding_smbus *smbus;
-	int rc;
 
 	mctp_set_log_stdio(MCTP_LOG_DEBUG);
 
-	mctp = mctp_init();
-	assert(mctp);
-	smbus = mctp_smbus_init();
-	assert(smbus);
+	p_ctx->mctp = mctp_init();
+	assert(p_ctx->mctp);
 
-	assert(strcmp(smbus->binding.name, "smbus") == 0);
-	assert(smbus->binding.version == 1);
-	assert(smbus->binding.tx != NULL);
+	p_ctx->bind_smbus = mctp_smbus_init();
 
-	rc = mctp_smbus_register_bus(smbus, mctp, TEST_EID);
+	assert(p_ctx->bind_smbus);
+
+	assert(strcmp(p_ctx->bind_smbus->binding.name, "smbus") == 0);
+	assert(p_ctx->bind_smbus->binding.version == 1);
+	assert(p_ctx->bind_smbus->binding.tx);
+}
+
+static void test_smbus_rx(mctp_eid_t src, void *data, void *msg, size_t len,
+			  bool tag_owner, uint8_t tag, void *ext)
+{
+	struct mctp_smbus_pkt_private *pkt_pvt =
+		((struct mctp_smbus_pkt_private *)ext);
+
+	uint8_t expected[] = { 0x8a, 0x0b, 0x51 };
+	int rc;
+
+	assert(src != 1);
+	assert(pkt_pvt->fd != -1);
+
+	assert(len == 3);
+
+	rc = memcmp(expected, (uint8_t *)msg, len);
+	assert(rc == 0);
+}
+
+static void destroy_smbus_test_ctx(struct smbus_test_ctx *p_ctx)
+{
+	mctp_smbus_free(p_ctx->bind_smbus);
+	__mctp_free(p_ctx->mctp);
+}
+
+static void run_smbus_rx_test(mctp_rx_fn rx_fn)
+{
+	int rc;
+	struct mctp_smbus_pkt_private ext_params;
+	struct mctp_pktbuf pkt;
+
+	init_smbus_test(&smbus_ctx);
+
+	ext_params.fd = stubbed_smbus_fd;
+	ext_params.mux_flags = IS_MUX_PORT;
+	ext_params.mux_hold_timeout = 1000;
+	ext_params.slave_addr = 0x11;
+
+	pkt.msg_binding_private = (void *)&ext_params;
+	pkt.next = NULL;
+
+	smbus_ctx.bind_smbus->rx_pkt = &pkt;
+
+	rc = mctp_smbus_register_bus(smbus_ctx.bind_smbus, smbus_ctx.mctp,
+				     TEST_EID);
 	assert(rc == 0);
 
-	/* cleanup */
-	mctp_smbus_free(smbus);
-	__mctp_free(mctp);
+	mctp_set_rx_all(smbus_ctx.mctp, rx_fn, NULL);
+
+	mctp_smbus_read(smbus_ctx.bind_smbus);
+
+	destroy_smbus_test_ctx(&smbus_ctx);
+}
+
+static void negative_text_smbus_rx(mctp_eid_t src, void *data, void *msg,
+				   size_t len, bool tag_owner, uint8_t tag,
+				   void *ext)
+{
+	/*change function call count*/
+	fn_call_cnt++;
+}
+
+static void run_smbus_tx_test(mctp_rx_fn rx_fn)
+{
+	int rc;
+	struct mctp_smbus_pkt_private ext_params;
+	struct mctp_pktbuf pkt;
+
+	init_smbus_test(&smbus_ctx);
+
+	ext_params.fd = stubbed_smbus_fd;
+	ext_params.mux_flags = IS_MUX_PORT;
+	ext_params.mux_hold_timeout = 1000;
+	ext_params.slave_addr = 0x11;
+
+	pkt.msg_binding_private = (void *)&ext_params;
+	pkt.next = NULL;
+
+	smbus_ctx.bind_smbus->rx_pkt = &pkt;
+
+	mctp_set_rx_all(smbus_ctx.mctp, rx_fn, NULL);
+
+	rc = mctp_smbus_register_bus(smbus_ctx.bind_smbus, smbus_ctx.mctp,
+				     TEST_EID);
+	assert(rc == 0);
+
+	assert(mctp_message_tx(smbus_ctx.mctp, TEST_EID, (void *)payload,
+			       sizeof(payload), true, 0,
+			       (void *)&ext_params) == 0);
+}
+
+static void run_smbus_rx_test_negative(mctp_rx_fn rx_fn)
+{
+	int rc;
+	struct mctp_smbus_pkt_private ext_params;
+	struct mctp_pktbuf pkt;
+
+	init_smbus_test(&smbus_ctx);
+
+	ext_params.fd = -1;
+	ext_params.mux_flags = IS_MUX_PORT;
+	ext_params.mux_hold_timeout = 1000;
+	ext_params.slave_addr = 0x11;
+
+	pkt.msg_binding_private = (void *)&ext_params;
+	pkt.next = NULL;
+
+	smbus_ctx.bind_smbus->rx_pkt = &pkt;
+
+	rc = mctp_smbus_register_bus(smbus_ctx.bind_smbus, smbus_ctx.mctp,
+				     TEST_EID);
+	assert(rc == 0);
+
+	mctp_set_rx_all(smbus_ctx.mctp, rx_fn, NULL);
+
+	mctp_smbus_read(smbus_ctx.bind_smbus);
+
+	/*calling again to access the call.*/
+	mctp_smbus_read(smbus_ctx.bind_smbus);
+
+	/*
+     * since for negative cases the call should not happen
+     * so this counter wont be incremented
+     */
+	assert(fn_call_cnt == 0);
+
+	destroy_smbus_test_ctx(&smbus_ctx);
+}
+
+int main(void)
+{
+	run_smbus_tx_test(NULL);
+	run_smbus_rx_test(test_smbus_rx);
+
+	/*Negetive test case*/
+	run_smbus_rx_test_negative(negative_text_smbus_rx);
 
 	return 0;
 }
